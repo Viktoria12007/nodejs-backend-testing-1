8b594df66e0132f4694b56ddca48f48603a003ff
Виктория Голубова <v.golubova@denexy.com>
2025-07-15 11:22:34 +0600

    Test cases implemented.

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -13,10 +13,12 @@ describe('PostsService', () => {
   });
 
   it('should add a new post', () => {
-    // реализуйте тест-кейс
+    const newPost = postsService.create(post);
+    expect(postsService.find(newPost.id)).toEqual(newPost);
   });
 
   it('should find a post', () => {
-    // реализуйте тест-кейс
+    const newPost = postsService.create(post);
+    expect(postsService.find(newPost.id)).toEqual(newPost);
   });
 });
